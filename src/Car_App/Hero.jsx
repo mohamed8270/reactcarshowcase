9fd744225d60fb10ca2f5e36643ff5d869792e6f
itshariharan <148336675+itshariharan@users.noreply.github.com>
2023-12-05 22:12:04 +0530

    hero section

---
+++ src/Car_App/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h1 className='text-blue'>Carsss..</h1>
+                <div>
+                    <button className='bg-drbl navbar-burger items-center font-bold'>Login</button>
+                </div>
+                <div>
+                    <h1 className='sm:[70px] text-[30px] text-drbl '>Tips and Tricks for <br/> Affordable Rental Car<br/> Deals! </h1>
+                    <p>Planning your trip well in advance allwos you <br/> to take advantage of early bird discounts <br/> and ensures a wider selection of vehicles. </p>
+                    <button className='text-drbl navbar-burger font-bold items-center '>BUY NOW</button>
+                    </div>
+                    <div>
+                        <img src='https://www.hdwallpapers.in/download/bmw_i7_xdrive60_excellence_the_first_edition_2022_4k_hd_cars-HD.jpg'/>
+                    </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        drbl: '#172554',
+        blue: '#1d4ed8',
+    },
   },
   plugins: [],
 }
 
+}
