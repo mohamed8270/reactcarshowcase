4a27cacc459795cc1c2c2c957662c1be90b72447
itshariharan <148336675+itshariharan@users.noreply.github.com>
2023-12-06 16:13:08 +0530

    Hero&NavBar
    css

--- src/Car_App/Hero.jsx
+++ src/Car_App/Hero.jsx
@@ -3,25 +3,22 @@ import React from 'react'
 const Hero = () => {
   return (
     
-        
-            <div className='flex p-3 '>
-                <h1 className='text-blue'>Carsss..</h1>
-                <div>
-                    <button className='bg-drbl navbar-burger items-center font-bold text-white'>Login</button>
-                </div>
-                <div className=' flex' >
-                    <div className=' p-3' >
-                    <h1 className='sm:[70px] text-[40px] text-drbl font-bold '>Tips and Tricks for <br/> Affordable Rental Car<br/> Deals! </h1>
-                    <p>Planning your trip well in advance allwos you <br/> to take advantage of early bird discounts <br/> and ensures a wider selection of vehicles. </p>
-                    <button className='bg-drbl text-white rounded-full  navbar-burger font-bold items-center '>BUY NOW</button>
-                   
-                    <div className=' flex h-[450px] p-1 '>
-                        <img src='https://www.hdwallpapers.in/download/bmw_i7_xdrive60_excellence_the_first_edition_2022_4k_hd_cars-HD.jpg'/>
+        <div>
+            
+                <div className=' sm:flex ' >
+                    <div className=' p-3 ml-14' >
+                        <h1 className='sm:[70px] text-[50px] font-bold text-drbl  '>Tips and Tricks for <br/> Affordable Rental Car </h1>
+                        <h1 className='sm:[70px] text-[50px] font-bold text-drbl md:underline-offset-4'> Deals! </h1>
+                        <p className='sm:[10px] text-[20px] mt-5 text-drbl'>Planning your trip well in advance allwos you <br/> to take advantage of early bird discounts <br/> and ensures a wider selection of vehicles. </p>
+                        <button className='bg-drbl text-white mt-9 rounded-full w-28  h-10 navbar-burger font-bold items-center '>BUY NOW</button>
                     </div>
-                    
+
+                    <div className=' flex-1 '>
+                        <img src='https://www.hdwallpapers.in/download/bmw_i7_xdrive60_excellence_the_first_edition_2022_4k_hd_cars-HD.jpg' />
                     </div>
+
                     </div>
-                   
+
             </div>
            
         
