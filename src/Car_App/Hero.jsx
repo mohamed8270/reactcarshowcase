355696adbab0eef3e62e7463af9faab740d777f0
itshariharan <148336675+itshariharan@users.noreply.github.com>
2023-12-06 11:19:46 +0530

    Hero
    pg

--- src/Car_App/Hero.jsx
+++ src/Car_App/Hero.jsx
@@ -2,24 +2,30 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-        <div>
-            <div>
+    
+        
+            <div className='flex p-3 '>
                 <h1 className='text-blue'>Carsss..</h1>
                 <div>
-                    <button className='bg-drbl navbar-burger items-center font-bold'>Login</button>
+                    <button className='bg-drbl navbar-burger items-center font-bold text-white'>Login</button>
                 </div>
-                <div>
-                    <h1 className='sm:[70px] text-[30px] text-drbl '>Tips and Tricks for <br/> Affordable Rental Car<br/> Deals! </h1>
+                <div className=' flex' >
+                    <div className=' p-3' >
+                    <h1 className='sm:[70px] text-[40px] text-drbl font-bold '>Tips and Tricks for <br/> Affordable Rental Car<br/> Deals! </h1>
                     <p>Planning your trip well in advance allwos you <br/> to take advantage of early bird discounts <br/> and ensures a wider selection of vehicles. </p>
-                    <button className='text-drbl navbar-burger font-bold items-center '>BUY NOW</button>
-                    </div>
-                    <div>
+                    <button className='bg-drbl text-white rounded-full  navbar-burger font-bold items-center '>BUY NOW</button>
+                   
+                    <div className=' flex h-[450px] p-1 '>
                         <img src='https://www.hdwallpapers.in/download/bmw_i7_xdrive60_excellence_the_first_edition_2022_4k_hd_cars-HD.jpg'/>
                     </div>
+                    
+                    </div>
+                    </div>
+                   
             </div>
-        </div>
-    </div>
+           
+        
+    
   )
 }
 
