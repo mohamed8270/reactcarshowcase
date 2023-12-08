af0795911df8efaf9058e03d5ee6046377af6ffc
AbinayaNagarajan162 <abinayanagarajan162@gmail.com>
2023-12-08 11:37:30 +0530

    Bodysection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Bodysection from './Components/Bodysection';
 
 function App() {
   return (
-    <div className="App bg bg-purple-700">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
- Abinaya
-        <p>
-          Edit <code>src/App.js</code> and hello save to reload.
-        <p className='text-[40px] text-[#242424] font-extralight'>
-          Edit <code>src/App.js</code> and save to reload main
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bodysection/>
     </div>
   );
 }
